6c67b66133ad04b3f75d3b330c1115101366f45a
Ashwani Bhatt <ashwanibhatt902@gmail.com>
2019-05-13 21:22:08 -0400

    Update PorPParameters.java

--- src/org/ieee/dyspansc/_1900/_5/scm/PorPParameters.java
+++ src/org/ieee/dyspansc/_1900/_5/scm/PorPParameters.java
@@ -30,25 +30,6 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
 
-/**
- * <p>Java class for PorPParameters complex type.
- * 
- * <p>The following schema fragment specifies the expected content contained within this class.
- * 
- * <pre>
- * &lt;complexType name="PorPParameters">
- *   &lt;complexContent>
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;sequence>
- *         &lt;element name="parameter" type="{http://www.w3.org/2001/XMLSchema}string" maxOccurs="unbounded"/>
- *       &lt;/sequence>
- *     &lt;/restriction>
- *   &lt;/complexContent>
- * &lt;/complexType>
- * </pre>
- * 
- * 
- */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "PorPParameters", propOrder = {
     "parameter"
