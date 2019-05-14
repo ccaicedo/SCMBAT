086af8707936a5bf41df06caa8dbfa5ee2e652af
Ashwani Bhatt <ashwanibhatt902@gmail.com>
2019-05-13 21:28:32 -0400

    Update SpectrumMask.java

--- src/org/ieee/dyspansc/_1900/_5/scm/SpectrumMask.java
+++ src/org/ieee/dyspansc/_1900/_5/scm/SpectrumMask.java
@@ -27,29 +27,6 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
-
-/**
- * <p>Java class for SpectrumMask complex type.
- * 
- * <p>The following schema fragment specifies the expected content contained within this class.
- * 
- * <pre>
- * &lt;complexType name="SpectrumMask">
- *   &lt;complexContent>
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;sequence>
- *         &lt;element name="hoppingData" type="{http://www.ieee.org/DyspanSC/1900/5/SCM}HoppingData" minOccurs="0"/>
- *         &lt;element name="resolutionBW" type="{http://www.w3.org/2001/XMLSchema}double"/>
- *         &lt;element name="scmMask" type="{http://www.ieee.org/DyspanSC/1900/5/SCM}SCMMask"/>
- *         &lt;element name="confidence" type="{http://www.ieee.org/DyspanSC/1900/5/SCM}Confidence" minOccurs="0"/>
- *       &lt;/sequence>
- *     &lt;/restriction>
- *   &lt;/complexContent>
- * &lt;/complexType>
- * </pre>
- * 
- * 
- */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "SpectrumMask", propOrder = {
     "hoppingData",
