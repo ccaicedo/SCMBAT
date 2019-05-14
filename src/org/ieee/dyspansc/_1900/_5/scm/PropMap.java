3761b8292da4f992baa7859c559b371b74563fa0
Ashwani Bhatt <ashwanibhatt902@gmail.com>
2019-05-13 21:22:17 -0400

    Update PropMap.java

--- src/org/ieee/dyspansc/_1900/_5/scm/PropMap.java
+++ src/org/ieee/dyspansc/_1900/_5/scm/PropMap.java
@@ -30,25 +30,6 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
 
-/**
- * <p>Java class for PropMap complex type.
- * 
- * <p>The following schema fragment specifies the expected content contained within this class.
- * 
- * <pre>
- * &lt;complexType name="PropMap">
- *   &lt;complexContent>
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;sequence>
- *         &lt;element name="propMapValue" type="{http://www.ieee.org/DyspanSC/1900/5/SCM}PropMapValue" maxOccurs="unbounded" minOccurs="5"/>
- *       &lt;/sequence>
- *     &lt;/restriction>
- *   &lt;/complexContent>
- * &lt;/complexType>
- * </pre>
- * 
- * 
- */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "PropMap", propOrder = {
     "propMapValue"
