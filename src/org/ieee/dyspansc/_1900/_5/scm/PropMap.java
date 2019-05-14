198eb58fe35c9dc86827447c4ce5a3f11b861480
Ashwani Bhatt <ashwanibhatt902@gmail.com>
2019-05-13 21:23:29 -0400

    Update PropMap.java

--- src/org/ieee/dyspansc/_1900/_5/scm/PropMap.java
+++ src/org/ieee/dyspansc/_1900/_5/scm/PropMap.java
@@ -39,28 +39,6 @@ public class PropMap {
     @XmlElement(required = true)
     protected List<PropMapValue> propMapValue;
 
-    /**
-     * Gets the value of the propMapValue property.
-     * 
-     * <p>
-     * This accessor method returns a reference to the live list,
-     * not a snapshot. Therefore any modification you make to the
-     * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the propMapValue property.
-     * 
-     * <p>
-     * For example, to add a new item, do as follows:
-     * <pre>
-     *    getPropMapValue().add(newItem);
-     * </pre>
-     * 
-     * 
-     * <p>
-     * Objects of the following type(s) are allowed in the list
-     * {@link PropMapValue }
-     * 
-     * 
-     */
     public List<PropMapValue> getPropMapValue() {
         if (propMapValue == null) {
             propMapValue = new ArrayList<PropMapValue>();
