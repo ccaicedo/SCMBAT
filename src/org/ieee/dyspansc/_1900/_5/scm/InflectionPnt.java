74811d4dcfbdf37e2d0c9ba26e84d619f2eb87d6
Ashwani Bhatt <ashwanibhatt902@gmail.com>
2019-05-13 21:16:45 -0400

    Update InflectionPnt.java

--- src/org/ieee/dyspansc/_1900/_5/scm/InflectionPnt.java
+++ src/org/ieee/dyspansc/_1900/_5/scm/InflectionPnt.java
@@ -27,26 +27,6 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlType;
 
 
-/**
- * <p>Java class for InflectionPnt complex type.
- * 
- * <p>The following schema fragment specifies the expected content contained within this class.
- * 
- * <pre>
- * &lt;complexType name="InflectionPnt">
- *   &lt;complexContent>
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;sequence>
- *         &lt;element name="frequency" type="{http://www.w3.org/2001/XMLSchema}double"/>
- *         &lt;element name="relativePower" type="{http://www.w3.org/2001/XMLSchema}double"/>
- *       &lt;/sequence>
- *     &lt;/restriction>
- *   &lt;/complexContent>
- * &lt;/complexType>
- * </pre>
- * 
- * 
- */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "InflectionPnt", propOrder = {
     "frequency",
