5003cd5c3c4930c3e396e8974c0ea8b565962e1e
Ashwani Bhatt <ashwanibhatt902@gmail.com>
2019-05-13 21:16:01 -0400

    Update FrequencyList.java

--- src/org/ieee/dyspansc/_1900/_5/scm/FrequencyList.java
+++ src/org/ieee/dyspansc/_1900/_5/scm/FrequencyList.java
@@ -30,31 +30,6 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
 
-/**
- * <p>Java class for FrequencyList complex type.
- * 
- * <p>The following schema fragment specifies the expected content contained within this class.
- * 
- * <pre>
- * &lt;complexType name="FrequencyList">
- *   &lt;complexContent>
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;sequence>
- *         &lt;element name="referenceFreq" maxOccurs="unbounded">
- *           &lt;simpleType>
- *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}double">
- *               &lt;minExclusive value="0"/>
- *             &lt;/restriction>
- *           &lt;/simpleType>
- *         &lt;/element>
- *       &lt;/sequence>
- *     &lt;/restriction>
- *   &lt;/complexContent>
- * &lt;/complexType>
- * </pre>
- * 
- * 
- */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "FrequencyList", propOrder = {
     "referenceFreq"
