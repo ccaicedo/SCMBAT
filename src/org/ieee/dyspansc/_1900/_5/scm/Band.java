0b3b820105ad2c45d5f4f20c6b37ab7304853a49
Ashwani Bhatt <ashwanibhatt902@gmail.com>
2019-05-13 21:14:20 -0400

    Update Band.java

--- src/org/ieee/dyspansc/_1900/_5/scm/Band.java
+++ src/org/ieee/dyspansc/_1900/_5/scm/Band.java
@@ -26,27 +26,6 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlType;
 
-
-/**
- * <p>Java class for Band complex type.
- * 
- * <p>The following schema fragment specifies the expected content contained within this class.
- * 
- * <pre>
- * &lt;complexType name="Band">
- *   &lt;complexContent>
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;sequence>
- *         &lt;element name="startFrequency" type="{http://www.w3.org/2001/XMLSchema}double"/>
- *         &lt;element name="endFrequency" type="{http://www.w3.org/2001/XMLSchema}double"/>
- *       &lt;/sequence>
- *     &lt;/restriction>
- *   &lt;/complexContent>
- * &lt;/complexType>
- * </pre>
- * 
- * 
- */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "Band", propOrder = {
     "startFrequency",
