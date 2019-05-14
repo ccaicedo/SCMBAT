05c245f343a74dc0af3709522b48f554f5e186d9
Ashwani Bhatt <ashwanibhatt902@gmail.com>
2019-05-13 21:12:30 -0400

    Update BTPRatedList.java

--- src/org/ieee/dyspansc/_1900/_5/scm/BTPRatedList.java
+++ src/org/ieee/dyspansc/_1900/_5/scm/BTPRatedList.java
@@ -30,25 +30,6 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
 
-/**
- * <p>Java class for BTPRatedList complex type.
- * 
- * <p>The following schema fragment specifies the expected content contained within this class.
- * 
- * <pre>
- * &lt;complexType name="BTPRatedList">
- *   &lt;complexContent>
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;sequence>
- *         &lt;element name="btpRating" type="{http://www.ieee.org/DyspanSC/1900/5/SCM}BTPRating" maxOccurs="unbounded"/>
- *       &lt;/sequence>
- *     &lt;/restriction>
- *   &lt;/complexContent>
- * &lt;/complexType>
- * </pre>
- * 
- * 
- */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "BTPRatedList", propOrder = {
     "btpRating"
