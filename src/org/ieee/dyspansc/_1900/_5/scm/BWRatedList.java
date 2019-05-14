aec551c37dd7da55061692319c3417f202119f1d
Ashwani Bhatt <ashwanibhatt902@gmail.com>
2019-05-13 21:12:58 -0400

    Update BWRatedList.java

--- src/org/ieee/dyspansc/_1900/_5/scm/BWRatedList.java
+++ src/org/ieee/dyspansc/_1900/_5/scm/BWRatedList.java
@@ -30,25 +30,6 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
 
-/**
- * <p>Java class for BWRatedList complex type.
- * 
- * <p>The following schema fragment specifies the expected content contained within this class.
- * 
- * <pre>
- * &lt;complexType name="BWRatedList">
- *   &lt;complexContent>
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;sequence>
- *         &lt;element name="bwRating" type="{http://www.ieee.org/DyspanSC/1900/5/SCM}BWRating" maxOccurs="unbounded"/>
- *       &lt;/sequence>
- *     &lt;/restriction>
- *   &lt;/complexContent>
- * &lt;/complexType>
- * </pre>
- * 
- * 
- */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "BWRatedList", propOrder = {
     "bwRating"
@@ -58,28 +39,7 @@ public class BWRatedList {
     @XmlElement(required = true)
     protected List<BWRating> bwRating;
 
-    /**
-     * Gets the value of the bwRating property.
-     * 
-     * <p>
-     * This accessor method returns a reference to the live list,
-     * not a snapshot. Therefore any modification you make to the
-     * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the bwRating property.
-     * 
-     * <p>
-     * For example, to add a new item, do as follows:
-     * <pre>
-     *    getBwRating().add(newItem);
-     * </pre>
-     * 
-     * 
-     * <p>
-     * Objects of the following type(s) are allowed in the list
-     * {@link BWRating }
-     * 
-     * 
-     */
+
     public List<BWRating> getBwRating() {
         if (bwRating == null) {
             bwRating = new ArrayList<BWRating>();
