8eac279f22f6bdd0e84c69c1c10ff3242709e460
Ashwani Bhatt <ashwanibhatt902@gmail.com>
2019-05-13 21:14:35 -0400

    Update BandList.java

--- src/org/ieee/dyspansc/_1900/_5/scm/BandList.java
+++ src/org/ieee/dyspansc/_1900/_5/scm/BandList.java
@@ -30,25 +30,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
 
-/**
- * <p>Java class for BandList complex type.
- * 
- * <p>The following schema fragment specifies the expected content contained within this class.
- * 
- * <pre>
- * &lt;complexType name="BandList">
- *   &lt;complexContent>
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;sequence>
- *         &lt;element name="band" type="{http://www.ieee.org/DyspanSC/1900/5/SCM}Band" maxOccurs="unbounded"/>
- *       &lt;/sequence>
- *     &lt;/restriction>
- *   &lt;/complexContent>
- * &lt;/complexType>
- * </pre>
- * 
- * 
- */
+
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "BandList", propOrder = {
     "band"
