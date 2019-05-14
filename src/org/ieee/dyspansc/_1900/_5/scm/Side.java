bd7567da79387c286b9be9cc796f10849f2caf62
Ashwani Bhatt <ashwanibhatt902@gmail.com>
2019-05-13 21:28:24 -0400

    Update Side.java

--- src/org/ieee/dyspansc/_1900/_5/scm/Side.java
+++ src/org/ieee/dyspansc/_1900/_5/scm/Side.java
@@ -28,26 +28,6 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
 
-/**
- * <p>Java class for Side complex type.
- * 
- * <p>The following schema fragment specifies the expected content contained within this class.
- * 
- * <pre>
- * &lt;complexType name="Side">
- *   &lt;complexContent>
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;sequence>
- *         &lt;element name="point" type="{http://www.ieee.org/DyspanSC/1900/5/SCM}Point"/>
- *         &lt;element name="sideAttenuation" type="{http://www.w3.org/2001/XMLSchema}double" minOccurs="0"/>
- *       &lt;/sequence>
- *     &lt;/restriction>
- *   &lt;/complexContent>
- * &lt;/complexType>
- * </pre>
- * 
- * 
- */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "Side", propOrder = {
     "point",
