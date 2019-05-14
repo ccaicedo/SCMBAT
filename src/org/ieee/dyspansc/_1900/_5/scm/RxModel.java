7d41655498d3c0745bc526ac9ebef9bbfe2abd67
Ashwani Bhatt <ashwanibhatt902@gmail.com>
2019-05-13 21:24:38 -0400

    Update RxModel.java

--- src/org/ieee/dyspansc/_1900/_5/scm/RxModel.java
+++ src/org/ieee/dyspansc/_1900/_5/scm/RxModel.java
@@ -30,44 +30,6 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
 
-/**
- * <p>Java class for RxModel complex type.
- * 
- * <p>The following schema fragment specifies the expected content contained within this class.
- * 
- * <pre>
- * &lt;complexType name="RxModel">
- *   &lt;complexContent>
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;sequence>
- *         &lt;element name="systemID" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
- *         &lt;element name="purpose">
- *           &lt;simpleType>
- *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
- *               &lt;enumeration value="AUTHORIZATION"/>
- *               &lt;enumeration value="CONSTRAINT"/>
- *               &lt;enumeration value="CONSUMPTION"/>
- *             &lt;/restriction>
- *           &lt;/simpleType>
- *         &lt;/element>
- *         &lt;element name="confidence" type="{http://www.ieee.org/DyspanSC/1900/5/SCM}Confidence" minOccurs="0"/>
- *         &lt;element name="referencePower" type="{http://www.ieee.org/DyspanSC/1900/5/SCM}ReferencePower" maxOccurs="unbounded"/>
- *         &lt;element name="underlayMask" type="{http://www.ieee.org/DyspanSC/1900/5/SCM}UnderlayMask" maxOccurs="unbounded"/>
- *         &lt;element name="scmPowerMap" type="{http://www.ieee.org/DyspanSC/1900/5/SCM}SCMPowerMap" maxOccurs="unbounded"/>
- *         &lt;element name="scmPropagationMap" type="{http://www.ieee.org/DyspanSC/1900/5/SCM}SCMPropagationMap" maxOccurs="unbounded"/>
- *         &lt;element name="intermodulationMask" type="{http://www.ieee.org/DyspanSC/1900/5/SCM}IntermodulationMask" maxOccurs="unbounded" minOccurs="0"/>
- *         &lt;element name="scmPlatform" type="{http://www.ieee.org/DyspanSC/1900/5/SCM}SCMPlatform" maxOccurs="unbounded" minOccurs="0"/>
- *         &lt;element name="scmLocation" type="{http://www.ieee.org/DyspanSC/1900/5/SCM}SCMLocation" maxOccurs="unbounded"/>
- *         &lt;element name="scmSchedule" type="{http://www.ieee.org/DyspanSC/1900/5/SCM}SCMSchedule" maxOccurs="unbounded"/>
- *         &lt;element name="scmPolicyOrProtocol" type="{http://www.ieee.org/DyspanSC/1900/5/SCM}SCMPolicyOrProtocol" maxOccurs="unbounded" minOccurs="0"/>
- *       &lt;/sequence>
- *     &lt;/restriction>
- *   &lt;/complexContent>
- * &lt;/complexType>
- * </pre>
- * 
- * 
- */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "RxModel", propOrder = {
     "systemID",
