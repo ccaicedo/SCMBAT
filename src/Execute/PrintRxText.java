e87ba9c0341b94f666f167dfbab2bb2a592f1e02
Ashwanibhatt902 <ashwanibhatt902@gmail.com>
2019-07-22 19:56:08 -0400

    new algo for Octave Powermap and Propogation Map

--- src/Execute/PrintRxText.java
+++ src/Execute/PrintRxText.java
@@ -42,170 +42,167 @@ import org.ieee.dyspansc._1900._5.scm.RxModel;
 
 import SCM_home.Home;
 
-public class PrintRxText extends PrintText{
-	
+public class PrintRxText extends PrintText {
+
 	final Logger logger = Logger.getLogger(PrintRxText.class);
-	public String printText(RxModel model, String SaveName){
-		
+
+	public String printText(RxModel model, String SaveName) {
+
 		logger.addAppender(Home.appender);
 		String warningMessage = "\n";
 		PrintWriter printfile;
-		try{
+		try {
 			System.out.println("Rx Printing");
 			logger.info("Rx Printing");
-			
-			//MethodAnalysis meth = new MethodAnalysis();
-		//	printfile = new PrintWriter (meth.getFilePath()+"Octave/" + SaveName);	
-			printfile = new PrintWriter (SaveName);
-			
-			//Create the file name
-	//		String savefileName = SaveName_getCurrentDate;
-			
-			//Adding the new Directory for adding the output files
-			//printfile = new PrintWriter ("CompatabilityAnalysis/" + SaveName);
-			int o=0;
-			
-			if(model.getUnderlayMask().get(o)!=null){
+
+			// MethodAnalysis meth = new MethodAnalysis();
+			// printfile = new PrintWriter (meth.getFilePath()+"Octave/" + SaveName);
+			printfile = new PrintWriter(SaveName);
+
+			// Create the file name
+			// String savefileName = SaveName_getCurrentDate;
+
+			// Adding the new Directory for adding the output files
+			// printfile = new PrintWriter ("CompatabilityAnalysis/" + SaveName);
+			int o = 0;
+
+			if (model.getUnderlayMask().get(o) != null) {
 				System.out.println("Underlay Mask Printing");
 				logger.info("Underlay Mask Printing");
 				printfile.println("# name: Rx_UnderlayMask");
 				printfile.println("# type: matrix");
 				printfile.println("# rows: 1");
-				List<InflectionPnt> infPoint = model.getUnderlayMask()
-						.get(o).getScmMask().getInflectionPnt();
+				List<InflectionPnt> infPoint = model.getUnderlayMask().get(o).getScmMask().getInflectionPnt();
 				String data = "";
-				for(int i=0; i<infPoint.size(); i++){
+				for (int i = 0; i < infPoint.size(); i++) {
 					data = data + infPoint.get(i).getFrequency() + " ";
 					data = data + infPoint.get(i).getRelativePower() + " ";
 				}
-				printfile.println("# columns: " + infPoint.size()*2);
-			    printfile.println(data);
-			    printfile.println("");
+				printfile.println("# columns: " + infPoint.size() * 2);
+				printfile.println(data);
 				printfile.println("");
-				
+				printfile.println("");
+
 				printfile.println("# name: Rx_ResBW");
 				printfile.println("# type: scalar");
-				if(!String.valueOf(model.getUnderlayMask().get(o).getResolutionBW()).equals(null)){
-				    printfile.println (model.getUnderlayMask().get(o).getResolutionBW());
+				if (!String.valueOf(model.getUnderlayMask().get(o).getResolutionBW()).equals(null)) {
+					printfile.println(model.getUnderlayMask().get(o).getResolutionBW());
 				}
-			    printfile.println("");
 				printfile.println("");
-				
-				if(model.getUnderlayMask().get(o).getRating()!=null){
-				
+				printfile.println("");
+
+				if (model.getUnderlayMask().get(o).getRating() != null) {
+
 					Rating ratedMask = model.getUnderlayMask().get(o).getRating();
-					
-					if(ratedMask.getRatedBW()!=0.0){
-						
+
+					if (ratedMask.getRatedBW() != 0.0) {
+
 						printfile.println("# name: Rx_BWRatedList");
 						printfile.println("# type: matrix");
-						printfile.println("# rows: 1");				
+						printfile.println("# rows: 1");
 						printfile.println("# columns: " + 2);
-					    printfile.println(ratedMask.getRatedBW() + " "+ 0.0);
-					    printfile.println("");
+						printfile.println(ratedMask.getRatedBW() + " " + 0.0);
+						printfile.println("");
 						printfile.println("");
 					}
-					
-					if(ratedMask.getBwRatedList()!=null){
+
+					if (ratedMask.getBwRatedList() != null) {
 						BWRatedList bwRatedList = ratedMask.getBwRatedList();
 						List<BWRating> bwRating = bwRatedList.getBwRating();
 						String bwData = "";
-						
-						for(int i=0; i<bwRating.size(); i++){
-							String data1 = String.valueOf(bwRating.get(i).getRatedBW());		
+
+						for (int i = 0; i < bwRating.size(); i++) {
+							String data1 = String.valueOf(bwRating.get(i).getRatedBW());
 							String data2 = String.valueOf(bwRating.get(i).getAdjustment());
 							bwData = bwData + data1 + " " + data2 + " ";
 						}
-						
+
 						printfile.println("# name: Rx_BWRatedList");
 						printfile.println("# type: matrix");
-						printfile.println("# rows: 1");				
-						printfile.println("# columns: " + bwRating.size()*2);
-					    printfile.println(bwData);
-					    printfile.println("");
+						printfile.println("# rows: 1");
+						printfile.println("# columns: " + bwRating.size() * 2);
+						printfile.println(bwData);
+						printfile.println("");
 						printfile.println("");
 					}
-					
-					if(ratedMask.getRatedBTP()!=0.0){
-					
+
+					if (ratedMask.getRatedBTP() != 0.0) {
+
 						printfile.println("# name: Rx_BTPRatedList");
 						printfile.println("# type: matrix");
-						printfile.println("# rows: 1");				
+						printfile.println("# rows: 1");
 						printfile.println("# columns: " + 2);
-					    printfile.println(ratedMask.getRatedBTP() +" "+0.0);
-					    printfile.println("");
+						printfile.println(ratedMask.getRatedBTP() + " " + 0.0);
+						printfile.println("");
 						printfile.println("");
 					}
-					
-					if(ratedMask.getBtpRatedList()!=null){
+
+					if (ratedMask.getBtpRatedList() != null) {
 						BTPRatedList btpRatedList = ratedMask.getBtpRatedList();
 						List<BTPRating> btpRating = btpRatedList.getBtpRating();
 						String btpData = "";
-												
-						for(int i=0; i<btpRating.size(); i++){
+
+						for (int i = 0; i < btpRating.size(); i++) {
 							String data1 = String.valueOf(btpRating.get(i).getBtp());
 							String data2 = String.valueOf(btpRating.get(i).getAdjustment());
 							btpData = btpData + data1 + " " + data2 + " ";
 						}
-						
+
 						printfile.println("# name: Rx_BTPRatedList");
 						printfile.println("# type: matrix");
-						printfile.println("# rows: 1");				
-						printfile.println("# columns: " + btpRating.size()*2);
-					    printfile.println(btpData);
-					    printfile.println("");
+						printfile.println("# rows: 1");
+						printfile.println("# columns: " + btpRating.size() * 2);
+						printfile.println(btpData);
+						printfile.println("");
 						printfile.println("");
 					}
-					
-					if(ratedMask.getDcRatedList()!=null){
-						
+
+					if (ratedMask.getDcRatedList() != null) {
+
 						DCRatedList dcRatedList = ratedMask.getDcRatedList();
 						List<DCRating> dcRating = dcRatedList.getDcRating();
 						String dcData = "";
-						
-						for(int i=0; i<dcRating.size(); i++){
+
+						for (int i = 0; i < dcRating.size(); i++) {
 							String data1 = String.valueOf(dcRating.get(i).getDc());
 							String data2 = String.valueOf(dcRating.get(i).getMaxDwellTime());
 							String data3 = String.valueOf(dcRating.get(i).getAdjustment());
 							dcData = dcData + data1 + " " + data2 + " " + data3 + " ";
 						}
-						
+
 						printfile.println("# name: Rx_DutyList");
 						printfile.println("# type: matrix");
-						printfile.println("# rows: 1");				
-						printfile.println("# columns: " + dcRating.size()*3);
-					    printfile.println(dcData);
-					    printfile.println("");
+						printfile.println("# rows: 1");
+						printfile.println("# columns: " + dcRating.size() * 3);
+						printfile.println(dcData);
 						printfile.println("");
-						
+						printfile.println("");
+
 					}
-					
-					if(ratedMask.getPorPIndex()!=0){
+
+					if (ratedMask.getPorPIndex() != 0) {
 						printfile.println("# name: Rx_Policy");
 						printfile.println("# type: scalar");
-					    printfile.println (ratedMask.getPorPIndex());
-					    printfile.println("");
+						printfile.println(ratedMask.getPorPIndex());
+						printfile.println("");
 						printfile.println("");
 					}
-					
-					
+
 				}
-				
-			}			
-			
+
+			}
+
 			warningMessage = warningMessage + printReferencePower(model.getReferencePower().get(o), printfile, "Rx");
-			warningMessage = warningMessage + printPowerMap(model.getScmPowerMap().get(o),printfile,"Rx");
-			printLocation(model.getScmLocation().get(o),printfile,"Rx");
-			printTime(model.getScmSchedule().get(o),printfile,"Rx");
-			
+			warningMessage = warningMessage + printPowerMap(model.getScmPowerMap().get(o), printfile, "Rx");
+			printLocation(model.getScmLocation().get(o), printfile, "Rx");
+			printTime(model.getScmSchedule().get(o), printfile, "Rx");
+
 			printfile.close();
-		}catch(Exception e){
-			
+		} catch (Exception e) {
+
 			warningMessage = warningMessage + "\nERROR - Couldn't save the Receiver data correctly";
-			//new Warn().setWarn("Error", "Couldn't save the Receiver data correctly");
+			// new Warn().setWarn("Error", "Couldn't save the Receiver data correctly");
 		}
 		return warningMessage;
 	}
 }
-
-
